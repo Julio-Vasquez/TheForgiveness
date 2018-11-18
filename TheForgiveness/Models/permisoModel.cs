be08147724ec54126d91dac354ccf657275e0327
David Calderon <davidnekocalderon@gmail.com>
2018-11-17 23:20:39 -0500

    faltaba este
    XD

---
+++ TheForgiveness/Models/permisoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheForgiveness.Models
+{
+    public class permisoModel
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string Url { get;set;}
+	`State
+	`Vista` INT NOT NULL
+    }
+}
