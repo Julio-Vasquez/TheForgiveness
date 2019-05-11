15bf7f7175a4b3184ac5a154ea76cda206611354
David Calderon <davidnekocalderon@gmail.com>
2019-05-11 17:21:35 -0500

    cambios
    nada que hacer

---
+++ TheForgiveness/Controllers/AuthorsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheForgiveness.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private Util.Util util = new Util.Util();
+        // GET: Profile
+        [HttpGet]
+        public ActionResult CreateAuthors()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UpdateAuthors()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DeleteAuthors()
+        {
+            return View();
+        }
+    }
+}
