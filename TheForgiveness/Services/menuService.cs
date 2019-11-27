1e372b52a19c76cb92408c80263946ba4102ba27
DarKPhuRioN <jualvalitube@gmail.com>
2019-11-27 13:05:04 -0500

    New dashboard

--- TheForgiveness/Services/menuService.cs
+++ TheForgiveness/Services/menuService.cs
@@ -17,7 +17,7 @@ namespace TheForgiveness.Services
         public System.Data.DataTable getMenu(string un)
         {
             rolService rol = new rolService();
-            return MySQL.Querys("SELECT v.Titulo AS 'Titulo_Vista', v.Icon AS 'Icono_Vista', p.Titulo AS 'Titulo_SubVista', p.Icon AS 'Icono_SubVista', p.Url AS 'Url_Carpeta', p.Archivo AS 'Url_Vista' FROM Vista AS v INNER JOIN Permiso AS p ON p.Vista = v.ID AND p.State = 'Activo' AND v.State = 'Activo' LEFT JOIN RolPermiso AS rp ON rp.Permiso = p.ID AND rp.State = 'Activo'  WHERE rp.Rol = " + rol.GetRole(un) + "; ");
+            return MySQL.Querys("SELECT p.State AS 'State', v.Titulo AS 'Titulo_Vista', v.Icon AS 'Icono_Vista', p.Titulo AS 'Titulo_SubVista', p.Icon AS 'Icono_SubVista', p.Url AS 'Url_Carpeta', p.Archivo AS 'Url_Vista' FROM Vista AS v INNER JOIN Permiso AS p ON p.Vista = v.ID  LEFT JOIN RolPermiso AS rp ON rp.Permiso = p.ID   WHERE rp.Rol =" + rol.GetRole(un) + "; ");
         }
     }
 }
