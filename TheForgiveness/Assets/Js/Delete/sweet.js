a442f511e1a172c8c4195a0c201773049b857454
DarKPhuRioN <jualvalitube@gmail.com>
2019-12-12 14:51:03 -0500

    new sweet alert

--- TheForgiveness/Assets/Js/Delete/sweet.js
+++ TheForgiveness/Assets/Js/Delete/sweet.js
@@ -1,23 +1,34 @@
 ﻿
-function del(e, typeText) {
+
+function del(e, typeText)
+{
     e.preventDefault();
     Swal.fire({
         title: 'Estas seguro?',
         text: "¡No podrás revertir esto!",
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, Eliminar.'
+        confirmButtonText: 'Yes, Eliminar!',
+        cancelButtonText: 'No, Cancelar!',
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        reverseButtons: true
     }).then((result) => {
         if (result.value) {
             Swal.fire(
                 'Eliminado!',
-                `El ${typeText} fue eliminado`,
+                `El registro de ${typeText} fue eliminado`,
                 'success'
             ).then(res => {
+                console.log(res);
                 document.getElementById('delete').submit();
             });
+        } else if ( result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire(
+                'Cancelado',
+                `Tu registro de ${typeText} esta seguro :)`,
+                'error'
+            );
         }
     });
 }
